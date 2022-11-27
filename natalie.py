ce58cee054594bf0e715008138a3a0c80c2bf6e9
nataliecly <nyu17@my.bcit.ca>
2022-11-26 21:33:44 -0800

    Add coordinates to make_character function

--- natalie.py
+++ natalie.py
@@ -1,9 +1,38 @@
+# def game():  # called from main
+#     rows = 10
+#     columns = 10
+#     board = make_board(rows, columns)
+#     character = make_character("Player name")
+#     achieved_goal = False
+#     while not achieved_goal:
+#         # Tell the user where they are
+#         describe_current_location(board, character)
+
+
 def make_character():
     character_name = input("Please enter a name for your character: ")
-    level = 1
-    attempts = 3
-    experience_points = 0
-    return {'name': character_name, 'level': level, 'attempts': attempts, 'XP': experience_points}
+    return {'name': character_name, 'level': 1, 'attempts': 3, 'XP': 0,
+            "X-coordinate": 0, "Y-coordinate": 0}
 
 
 print(make_character())
+
+
+# def make_board(rows, columns):
+#     board = {}
+#     for row in range(rows):
+#         for column in range(columns):
+#             board[(row, column)] = "x"
+#     return board
+#
+#
+# print(make_board(10, 10))
+
+
+# def describe_current_location(board):
+#     board += '\n'
+#     # character = 1, 0
+#     print(board)
+#
+#
+# print(describe_current_location(10, 10))
