ae435e01a5e537047f02c3cd56d2917854c234af
Jason Shi <46632519+jaason-shi@users.noreply.github.com>
2022-11-26 20:55:21 -0800

    add make board code for print function

--- jason.py
+++ jason.py
@@ -6,9 +6,7 @@ def make_board(rows, columns):
             if column == 1:
                 print("\n")
             print(board)
-
-
-            # print(row, column)# ["Welcome to the pit of doom", None]
+    # print(row, column)# ["Welcome to the pit of doom", None]
     return board
 
 print(make_board(10,10))
