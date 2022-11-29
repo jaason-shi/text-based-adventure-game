a6fbe94756b9a6674a2082598acaa562b289ed73
jaason-shi <jasons8002@gmail.com>
2022-11-29 12:24:30 -0800

    read json files and convert strings to dictionary

--- jason.py
+++ jason.py
@@ -1,12 +1,35 @@
-def make_board(rows, columns):
-    board = {}
-    for row in range(rows):
-        for column in range(columns):
-            board[(row, column)] = "Johnson"
-            if column == 1:
-                print("\n")
-            print(board)
-    # print(row, column)# ["Welcome to the pit of doom", None]
-    return board
+import json
+import io
 
-print(make_board(10,10))
+
+get_riddles = open('riddles.json', 'r')
+riddles = json.load(get_riddles)
+# for riddle in riddles.values():
+print(riddles)
+
+get_rooms = open('rooms.json', 'r')
+rooms = json.load(get_rooms)
+# for room in rooms.values():
+print(rooms)
+
+get_riddles.close()
+print(type(riddles))
+
+
+
+
+
+# def make_board(rows, columns):
+#     board = {}
+#     for row in range(rows):
+#         for column in range(columns):
+#             board[(row, column)] = "Johnson"
+#             if column == 1:
+#                 print("\n")
+#             print(board)
+#     # print(row, column)# ["Welcome to the pit of doom", None]
+#     return board
+#
+# print(make_board(10,10))
+
+
