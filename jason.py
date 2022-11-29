2d14769619af33be91010f280929a9a0c04d5203
jaason-shi <jasons8002@gmail.com>
2022-11-29 12:35:50 -0800

    update code for json files to only show the dictionary

--- jason.py
+++ jason.py
@@ -4,19 +4,15 @@ import io
 
 get_riddles = open('riddles.json', 'r')
 riddles = json.load(get_riddles)
-# for riddle in riddles.values():
-print(riddles)
+for riddle in riddles['riddles_list']:
+    print(riddle)
 
 get_rooms = open('rooms.json', 'r')
 rooms = json.load(get_rooms)
-# for room in rooms.values():
-print(rooms)
+for room in rooms['rooms_list']:
+    print(room)
 
 get_riddles.close()
-print(type(riddles))
-
-
-
 
 
 # def make_board(rows, columns):
@@ -31,5 +27,3 @@ print(type(riddles))
 #     return board
 #
 # print(make_board(10,10))
-
-
