0cc399e7f5fab54ab60d026ca134b593f013c54b
jaason-shi <jasons8002@gmail.com>
2022-12-02 17:16:42 -0800

    create docstring for get_player_answer and get_correct_answer

--- game.py
+++ game.py
@@ -194,11 +194,26 @@ def make_riddle(riddle_number: int) -> str:
 
 
 def get_player_answer() -> str:
+    """
+    Return user input.
+
+    :postcondition: prints the user input
+    :return: the user input as a string
+    """
     print("Please enter your answer here: ")
     return input()
 
 
 def get_correct_answer(riddle_number: int) -> str:
+    """
+    Return the value as a string from dictionary key "answer" by using the riddle_number as the index.
+
+    :param riddle_number: an integer
+    :precondition: riddle_number must be an integer from [0, 12]
+    :postcondition: correctly returns value as a string from dictionary key "answer"
+    :postcondition: riddle_number is unchanged
+    :return: a string from the dictionary key "answer" by using the riddle_number as the index
+    """
     get_riddles = open('riddles.json', 'r')
     riddles = json.load(get_riddles)
     correct_answer = riddles['riddles_list'][riddle_number]["answer"]
@@ -214,6 +229,7 @@ def check_player_answer(player_answer: str, correct_answer: str) -> bool:
     :precondition: player_answer must be a string
     :precondition: correct_answer must be a string
     :postcondition: correctly determines if player_answer is same as correct_answer
+    :postcondition: player_answer and correct_answer are unchanged
     :return: True if player_answer is same as correct_answer, else False
     """
     if player_answer == correct_answer:
