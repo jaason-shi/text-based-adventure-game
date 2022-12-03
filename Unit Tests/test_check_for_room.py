3aad8890baf367e34305c3a9b08e31040f6b8924
nataliecly <nyu17@my.bcit.ca>
2022-12-02 23:34:36 -0800

    Amend unit tests for check_for_room

--- Unit Tests/test_check_for_room.py
+++ Unit Tests/test_check_for_room.py
@@ -4,14 +4,14 @@ from game import check_for_room
 
 class TestCheckForRoom(TestCase):
     def test_check_for_room_character_not_in_room(self):
-        test_character = {'Name': 'chris', 'Grade': 1, 'Attempts': 3, 'XP': 0, 'x-coordinate': 0, 'y-coordinate': 0}
+        test_character = {'Name': 'Chris', 'Grade': 1, 'Attempts': 3, 'XP': 0, 'x-coordinate': 0, 'y-coordinate': 0}
         test_board = {(0, 0): 'hallway', (0, 1): 'art room'}
         expected = False
         actual = check_for_room(test_character, test_board)
         self.assertEqual(expected, actual)
 
     def test_check_for_room_is_false_character_is_in_room(self):
-        test_character = {'Name': 'chris', 'Grade': 1, 'Attempts': 3, 'XP': 0, 'x-coordinate': 0, 'y-coordinate': 1}
+        test_character = {'Name': 'Chris', 'Grade': 1, 'Attempts': 3, 'XP': 0, 'x-coordinate': 0, 'y-coordinate': 1}
         test_board = {(0, 0): 'hallway', (0, 1): 'art room'}
         expected = True
         actual = check_for_room(test_character, test_board)
